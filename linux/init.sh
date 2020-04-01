b0ecd72ba18ecf6a78461fdd4f38f1171b6b3834
taccisum <liaojinfeng@deepexi.com>
2020-04-01 17:14:50 +0800

    fix: '-eq' instead '='

--- linux/init.sh
+++ linux/init.sh
@@ -15,7 +15,7 @@ function install_oh_my_zsh(){
     else
         sh local/zsh_install.sh
     fi
-    if [ ! $? = 0 ];then
+    if [ ! $? -eq 0 ];then
         echo 'fail to install oh my zsh.'
         exit 1
     fi
