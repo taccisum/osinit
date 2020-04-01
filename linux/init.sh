9313c2eb39bae98d535fa2edcd490642c31614ed
taccisum <liaojinfeng@deepexi.com>
2020-04-01 15:50:46 +0800

    fix: autosuggesstions install failed

--- linux/init.sh
+++ linux/init.sh
@@ -16,24 +16,6 @@ function install_oh_my_zsh(){
     fi
 }
 
-# $1: 要安装的插件
-# $2：zsh插件启动命令 (>> ~/.zshrc)
-function install_zsh_plugin(){
-    install $1
-    # check if zsh installed
-    if [ -f /bin/zsh ] ;then
-        row=$(cat ~/.zshrc | grep "$2")
-        if [ ! -z "$row" ];then
-            echo '检测到zsh已启用'$1'插件，将不做任何修改'
-        else
-            echo "$2" >> ~/.zshrc
-        fi
-    else
-        echo '检测到zsh未安装，将自动进行安装'
-        install_zsh
-    fi
-}
-
 function install(){
     yum install $1 -y
 }
@@ -54,13 +36,9 @@ function main(){
     done
 
     # zsh plugins
-    install_zsh_plugin \
-        'autojump' \
-        "[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh"
+    install autojump-zsh
 
-    install_zsh_plugin \
-        'zsh-syntax-highlighting' \
-        "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
 main $@
